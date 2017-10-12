4fbd49fe2162891c9c14d88ee7e6327392f3bc56
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-10-12 17:22:56 -0400

    Set up components for workspace and nav bar

--- planit/src/App.js
+++ planit/src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import NavBar from './components/nav_bar/index.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to Planit</h1>
+        	<NavBar />
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
