7a85365d2b96ee14432d30bfee494413ad01db7c
Samuel Lee <sleeinsuk@gmail.com>
2017-10-16 14:09:01 -0400

    Added migration for create user

---
+++ planit_server/priv/repo/migrations/20171016173251_create_user.exs
@@ -0,0 +1,17 @@
+defmodule PlanIt.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :fname, :string
+      add :lname, :string
+      add :email, :string
+      add :username, :string
+      add :birthday, :integer
+
+      timestamps
+    end
+
+    create unique_index(:users, [:email, :username])
+  end
+end
