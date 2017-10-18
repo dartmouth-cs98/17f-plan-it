aca6bb883ed618bb3f61050d1ecd1384c2c17a06
Samuel Lee <sleeinsuk@gmail.com>
2017-10-18 19:39:20 -0400

    forgotten file associated with previous commit

--- planit_server/web/models/Trip.ex
+++ planit_server/web/models/Trip.ex
@@ -1,8 +1,6 @@
 defmodule PlanIt.Trip do
   use Ecto.Schema
 
-
-
   schema "trip" do
     belongs_to :day, PlanIt.Day
 
@@ -10,9 +8,8 @@ defmodule PlanIt.Trip do
     field :intensity, :integer
     field :visibility, :boolean
     field :upvotes, :integer
-    field :user_id, has_many(:user, PlanIt.User)
-    field :first_card_id, has_many(:card, PlanIt.Card)
-    field :last_card_id, has_many(:card, PlanIt.Card)
+    has_many(:user, PlanIt.User)
+    has_many(:card, PlanIt.Card)
 
     timestamps()
   end
