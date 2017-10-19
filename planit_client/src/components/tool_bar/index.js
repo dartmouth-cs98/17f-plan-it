1cfb0c4b242dd3961e53ae1b3d4f4ad66474f615
Robert Sayegh <sayeghro@gmail.com>
2017-10-19 15:55:14 -0400

    toolbar

--- planit_client/src/components/tool_bar/index.js
+++ planit_client/src/components/tool_bar/index.js
@@ -5,7 +5,38 @@ export default class Toolbar extends Component {
 	render() {
 		return (
 			<div id='tool-bar'>
-				Toolbar
+				<div id='tool-bar-left'>
+					<div id='tool-bar-cost-box' class='tool-bar-left-flex-item'>
+						<div class='tool-bar-header-text'>
+							Avg Cost
+						</div>
+						<div class='tool-bar-text'>
+							// TODO: Money sign Component probably needed to calculate the number of dollar signs
+							$$$$
+						</div>
+					</div>
+					<div id='tool-bar-travel-time-box' class='tool-bar-left-flex-item'>
+						<div class='tool-bar-header-text'>
+							Travel Time
+						</div>
+						<div class='tool-bar-text'>
+							5 Hours
+						</div>
+					</div>
+				</div>
+				<div id='tool-bar-center'>
+					<div id='toolbar-trip-title'>
+						Southeast Asia
+					</div>
+				</div>
+				<div id='tool-bar-right'>
+					<a href="#" id='toolbar-share-button' class='tool-bar-header-text tool-bar-right-flex-item'>
+						Share
+					</a>
+					<a href="#" id='toolbar-export-button' class='tool-bar-header-text tool-bar-right-flex-item'>
+						Export
+					</a>
+				</div>
 			</div>
 		)
 	}
