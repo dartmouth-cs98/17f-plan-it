3302eed365a65ee6f9e8ed4fc636ac57b99c2637
Samuel Lee <sleeinsuk@gmail.com>
2017-10-24 18:03:12 -0400

    Parner w/Helen and Jiyun. Added user controller

--- planit_server/web/controllers/user_controller.ex
+++ planit_server/web/controllers/user_controller.ex
@@ -8,29 +8,62 @@ defmodule PlanIt.UserController do
   use PlanIt.Web, :controller
 
   def index(conn, _params) do
-    #query = from u in PlanIt.User,
-    # preload: [{:trip, [{:card, :card_travel}, :day]}]
-    #users = Repo.all(query)
-
-    #users = PlanIt.User |> Repo.all |> Repo.preload(trip: :card) |> Repo.preload(trip: :day) |> Repo.preload(:trip [card: :card_travel])
-    users = PlanIt.User
-            |> Repo.all
+    users = PlanIt.User |> Repo.all
 
     json conn, users
   end
 
-  def create_sample(conn, _params) do
+  def single_user(conn, %{"user_id" => user_id} = params) do
+    if user_id == nil do
+      json conn, "this is bad"
+    end
 
-    Repo.insert!(%User{fname: "Sam", lname: "Lee", email: "samlee@example.com", username: "slee", birthday: ~D[1996-12-31]})
+    user = (from u in User,
+      where: u.id == ^user_id,
+      select: u
+    ) |> Repo.one
+
+    json conn, user
+  end
+
+  def create_sample(conn, _params) do
+    Repo.insert!(%User{
+      fname: "Sam",
+      lname: "Lee",
+      email: "samlee@example.com",
+      username: "slee",
+      birthday: ~D[1996-12-31]})
 
     Repo.insert!(%Travel{type: "walking", duration: ~T[10:10:00]})
 
-    Repo.insert!(%Card{type: "restaurant", name: "Pine", city: "Hanover", country: "USA", address: "Corner of the Green", start_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"), end_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"), day_number: 1, travel_id: 1})
-
-    Repo.insert!(%Card{type: "exercise", name: "Alumni Gym", city: "Hanover", country: "USA", address: "In front of East Wheelock", start_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"), end_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"), day_number: 1, travel_id: 1})
-
-
     Repo.insert!(%Trip{name: "test trip", user_id: 1})
+
+    Repo.insert!(%Card{
+      type: "restaurant",
+      name: "Pine",
+      city: "Hanover",
+      country: "USA",
+      address: "Corner of the Green",
+      start_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"),
+      end_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"),
+      day_number: 1,
+      travel_id: 1,
+      trip_id: 1
+    })
+
+    Repo.insert!(%Card{
+      type: "exercise",
+      name: "Alumni Gym",
+      city: "Hanover",
+      country: "USA",
+      address: "In front of East Wheelock",
+      start_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"),
+      end_time: DateTime.from_naive!(~N[2016-05-24 13:26:08.003], "Etc/UTC"),
+      day_number: 1,
+      trip_id: 1
+    })
+
+
 
     json conn, []
   end
