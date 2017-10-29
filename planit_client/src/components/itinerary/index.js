8f76bbc56f975411a816e17fb0d3d17b0f373ee8
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-10-28 21:44:45 -0400

    Set up for itinerary development with D3 and fixture data

--- planit_client/src/components/itinerary/index.js
+++ planit_client/src/components/itinerary/index.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react'
 import $ from 'jquery'
-import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import {scaleLinear} from 'd3-scale'
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 import './index.scss'
+
+const ITINERARY = [
+	{
+		id: 851093,
+		type: 'Attraction',
+		name: 'Thai Taiwanese Embassy',
+		description: 'Two become one',
+		start_datetime: '2017-10-28T10:30:15+00:00',
+		end_datetime: '2017-10-28T11:30:15+00:00',
+		next: 193048
+	},
+	{
+		id: 193048,
+		type: 'Travel', 
+		next: 148290
+	},
+	{
+		id: 148290,
+		type: 'Attraction',
+		name: 'Thai the Knot',
+		description: 'The perfect venue for weddings of all different kinds',
+		start_datetime: '2017-10-28T11:45+00:00',
+		end_datetime: '2017-10-28T12:45+00:00'
+	}
+]
 
 export default class Itinerary extends Component {
 	constructor(props) {
@@ -43,6 +69,14 @@ export default class Itinerary extends Component {
 						/>
 					}
 				/>
+			</div>
+		)
+	}
+
+	renderList() {
+		return (
+			<div className='card-list'>
+
 			</div>
 		)
 	}
