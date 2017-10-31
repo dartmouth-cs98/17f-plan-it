090dfa7adb1dc58434b6f0a748f4efb9f5dfd8e9
jeffreysgao <jeffrey.s.gao@gmail.com>
2017-10-31 14:35:51 -0400

    Got 'remove card' functionality up and running on fixture data (need endpoints)

--- planit_client/src/components/itinerary/index.js
+++ planit_client/src/components/itinerary/index.js
@@ -5,67 +5,13 @@ import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card'
 import FlatButton from 'material-ui/FlatButton'
 import './index.scss'
 
-const ITINERARY = [
-	{
-		id: 851093,
-		type: 'Attraction',
-		name: 'Thai Taiwanese Embassy',
-		description: 'Two become one',
-		start_datetime: 'October 28, 2017 9:45:00',
-		end_datetime: 'October 28, 2017 10:30:00',
-		next: 193048
-	},
-	{
-		id: 193048,
-		type: 'Travel', 
-		next: 148290
-	},
-	{
-		id: 148290,
-		type: 'Attraction',
-		name: 'Thai the Knot',
-		description: 'The perfect venue for weddings of all different kinds',
-		start_datetime: 'November 14, 2017 10:45:00',
-		end_datetime: 'November 14, 2017 11:45:00'
-	}
-]
-
 export default class Itinerary extends Component {
-	constructor(props) {
-		super(props) 
-
-		this.state = {
-			day: 1, 
-			data: [
-				{
-					date: 'November 14', 
-					cards: ITINERARY
-				},
-				{
-					date: 'November 15', 
-					cards: ITINERARY
-				}
-			]
-		}
-
-		this.dayForward = this.dayForward.bind(this)
-		this.dayBackward = this.dayBackward.bind(this)
-	}
-
-	dayForward() {
-		this.setState({ day: Math.min(this.state.day + 1, this.state.data.length) })
-	}
-
-	dayBackward() {
-		this.setState({ day: Math.max(this.state.day - 1, 1) })
-	}
-
 	renderBackButton() {
-		if (this.state.day > 1) {
+		if (this.props.backArrow) {
 			return (
 				<FlatButton
 					className='left-button'
-					onClick={this.dayBackward}
+					onClick={this.props.dayBackward}
 					icon={
 						<i
 							class='fa fa-caret-left'
@@ -78,13 +24,11 @@ export default class Itinerary extends Component {
 	}
 
 	renderForwardButton() {
-		console.log(this.state.data.length, this.state.day)
-
-		if (this.state.day < this.state.data.length) {
+		if (this.props.forwardArrow) {
 			return (
 				<FlatButton
 					className='right-button'
-					onClick={this.dayForward}
+					onClick={this.props.dayForward}
 					icon={
 						<i
 							class='fa fa-caret-right'
@@ -102,7 +46,7 @@ export default class Itinerary extends Component {
 				{this.renderBackButton()}
 				<FlatButton
 					className='itinerary-label'
-					label={`Day ${this.state.day}: ${this.state.data[this.state.day - 1].date}`}
+					label={`Day ${this.props.day}: ${this.props.itinerary.date}`}
 				/>
 				{this.renderForwardButton()}
 			</div>
@@ -115,7 +59,7 @@ export default class Itinerary extends Component {
 			.domain([0, 1440])
 			.range([0, 750])
 
-		const cards = this.state.data[this.state.day - 1].cards.map((card) => {
+		const cards = this.props.itinerary.cards.map((card) => {
 			if (card.type === 'Attraction') {
 				const start = new Date(card.start_datetime)
 				const end = new Date(card.end_datetime)
@@ -128,6 +72,9 @@ export default class Itinerary extends Component {
 						key={card.id}
 						name={card.name}
 						description={card.description}
+						remove={() => {
+							this.props.removeCard(card.id)
+						}}
 						style={{height: `${height}px`}}
 					/>
 				)
@@ -164,7 +111,13 @@ class Item extends Component {
 			      showExpandableButton={true}
 			    />
 			    <CardActions>
-			    	<FlatButton label='Remove' />
+			    	<FlatButton 
+			    		label='Remove'
+			    		onClick={this.props.remove}
+		    		/>
+		    		<FlatButton
+		    			label='Add After'
+	    			/>
 		    	</CardActions>
 			    <CardText expandable={true}>
 			      {this.props.description}
--- planit_client/src/components/workspace/index.js
+++ planit_client/src/components/workspace/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import _ from 'lodash'
 import Toolbar from '../tool_bar/index.js'
 import Suggestions from '../suggestions/index.js'
 import Itinerary from '../itinerary/index.js'
@@ -6,17 +7,130 @@ import NavBar from '../nav_bar/index.js'
 import Map from '../map/index.js'
 require('./index.scss')
 
+const ITINERARY = [
+	{
+		date: 'November 14',
+		cards: [
+			{
+				id: 851093,
+				type: 'Attraction',
+				name: 'Thai Taiwanese Embassy',
+				description: 'Two become one',
+				start_datetime: 'October 28, 2017 9:45:00',
+				end_datetime: 'October 28, 2017 10:30:00',
+				next: 193048
+			},
+			{
+				id: 193048,
+				type: 'Travel', 
+				next: 148290
+			},
+			{
+				id: 148290,
+				type: 'Attraction',
+				name: 'Thai the Knot',
+				description: 'The perfect venue for weddings of all different kinds',
+				start_datetime: 'November 14, 2017 10:45:00',
+				end_datetime: 'November 14, 2017 11:45:00'
+			}
+		]
+	},
+	{
+		date: 'November 15',
+		cards: [
+			{
+				id: 851093,
+				type: 'Attraction',
+				name: 'Thai Taiwanese Embassy',
+				description: 'Two become one',
+				start_datetime: 'October 28, 2017 9:45:00',
+				end_datetime: 'October 28, 2017 10:30:00',
+				next: 193048
+			},
+			{
+				id: 193048,
+				type: 'Travel', 
+				next: 148290
+			},
+			{
+				id: 148290,
+				type: 'Attraction',
+				name: 'Thai the Knot',
+				description: 'The perfect venue for weddings of all different kinds',
+				start_datetime: 'November 14, 2017 10:45:00',
+				end_datetime: 'November 14, 2017 11:45:00'
+			}
+		]
+	}
+]
 
 export default class Workspace extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			day: 1,
+			itinerary: ITINERARY[0]
+		}
+
+		this.addCard = this.addCard.bind(this)
+		this.removeCard = this.removeCard.bind(this)
+		this.dayForward = this.dayForward.bind(this)
+		this.dayBackward = this.dayBackward.bind(this)
+	}
+
+	addCard(card) {
+		this.setState({	cards: this.state.cards.concat([card]) })
+	}
+
+	removeCard(id) {
+		this.setState({
+			itinerary: {
+				date: this.state.itinerary.date,
+				cards: _.filter(this.state.itinerary.cards, (card) => { 
+					return card.id !== id
+				})
+			}
+		})
+
+		// api call to remove card from itinerary
+	}
+
+	dayForward() {
+		const day = Math.min(this.state.day + 1, ITINERARY.length)
+		this.setState({ 
+			day,
+			itinerary: ITINERARY[day - 1]
+		})
+	}
+
+	dayBackward() {
+		const day = Math.max(this.state.day - 1, 1)
+
+		this.setState({ 
+			day,
+			itinerary: ITINERARY[day - 1]
+		})
+	}
+
 	render() {
+							// <Map isInfoOpen={false} isMarkerShown={true} MarkerPosition={{ lat: 43.704441, lng: -72.288694 }} center={{ lat: 43.704441, lng: -72.288694 }} infoMessage="Hello From Dartmouth"/>
+
 		return (
 			<div id='workspace'>
 				<NavBar background={'globe_background'}/>
 				<Toolbar />
 				<div className='planner'>
-					<Suggestions />
-					<Itinerary />
-					<Map isInfoOpen={false} isMarkerShown={true} MarkerPosition={{ lat: 43.704441, lng: -72.288694 }} center={{ lat: 43.704441, lng: -72.288694 }} infoMessage="Hello From Dartmouth"/>
+					<Suggestions addCard={this.addCard}	/>
+					<Itinerary 
+						itinerary={this.state.itinerary}
+						day={this.state.day}
+						removeCard={this.removeCard}
+						dayForward={this.dayForward}
+						dayBackward={this.dayBackward}
+						backArrow={this.state.day > 1}
+						forwardArrow={this.state.day < ITINERARY.length}
+					/>
 				</div>
 			</div>
 		)
