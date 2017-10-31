ebacfb6eb35b3565a8f62812000c8005b2893be6
Emily <emily.j.lin.18@dartmouth.edu>
2017-10-31 18:36:49 -0400

    preliminary dummy reducer/actions

--- planit_client/src/index.js
+++ planit_client/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-require('./index.scss')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import thunk from 'redux-thunk'
+import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { ActionTypes } from './actions'
+import reducers from './reducers'
+import './index.scss'
+import registerServiceWorker from './registerServiceWorker';
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f,
+));
+// const token = localStorage.getItem('token');
+// if (token) {
+//   store.dispatch({ type: ActionTypes.AUTH_USER });
+// }
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
+
