7cb1485106e388bd15b773b44ab6de6c3e43f0af
Samuel Lee <sleeinsuk@gmail.com>
2017-11-05 15:55:54 -0500

    Partner with Helen and Jiyun. Added description, url and photo url firleds to cards. Added photo url to trips. Updated seed data

--- planit_server/web/models/Card.ex
+++ planit_server/web/models/Card.ex
@@ -17,6 +17,10 @@ defmodule PlanIt.Card do
     field :end_time, :utc_datetime
     field :day_number, :integer
 
+    field :description, :string
+    field :photo_url, :string
+    field :url, :string
+
     field :travel_type, :string
     field :travel_duration, :integer
 
@@ -26,7 +30,7 @@ defmodule PlanIt.Card do
   def changeset(card, params) do
     #validate format of lat and long
     card
-    |> cast(params, [:type, :name, :city, :country, :address, :lat, :long, :start_time, :end_time, :day_number, :travel_type, :travel_duration])
+    |> cast(params, [:type, :name, :city, :country, :address, :lat, :long, :start_time, :end_time, :day_number, :description, :photo_url, :url, :travel_type, :travel_duration])
     |> cast(params, [:trip_id])
     |> validate_required([:name, :country, :day_number])
   end
