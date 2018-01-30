c682883797f972f8606bd2e47e57e53e40f6a954
Emily <emily.j.lin.18@dartmouth.edu>
2018-01-30 13:19:34 -0500

    Took care of null trip list handling

--- src/components/explore/index.js
+++ src/components/explore/index.js
@@ -38,6 +38,31 @@ class Explore extends Component {
 		this.props.fetchPublishedTrips()
 	}
 
+	renderTripList(trips, max_trips, title) {
+		const explore_settings = {
+	      dots: false,
+	      infinite: false,
+	      speed: 500,
+	      slidesToShow: 4,
+	      slidesToScroll: 4,
+	      nextArrow: <NextArrow/>,
+	      prevArrow: <PrevArrow/>
+    	};
+
+		if (trips.length != 0) {
+			return (
+				<div>
+				<div className='title'>{title}</div>
+				<Slider {...explore_settings} className='explore_slider'>
+					{this.renderPublished(trips, max_trips)}
+				</Slider>
+				</div>
+			)
+		} else {
+			return
+		}
+	}
+
 	renderPublished(trips, max_trips) {
 		let image = 'https://media.gadventures.com/media-server/cache/38/89/3889f45752d19449f909300bb0b7ad02.jpg'
 		let counter = 0
@@ -58,39 +83,17 @@ class Explore extends Component {
 	}
 
 	render() {
-		const explore_settings = {
-	      dots: false,
-	      infinite: false,
-	      speed: 500,
-	      slidesToShow: 4,
-	      slidesToScroll: 4,
-	      nextArrow: <NextArrow/>,
-	      prevArrow: <PrevArrow/>
-    	};
-
-    	let hasPublishedTrips = this.props.publishedTrips.length != 0 
+    	let hasPublishedTrips = this.props.publishedTrips.length != 0
 
     	if (hasPublishedTrips) {
 			return (
 			<div>
 				<NavBar background={'road_trip_background'}/>
 				<div>
-					<div className='title'>Recently Published Trips</div>
-					<Slider {...explore_settings} className='explore_slider'>
-						{this.renderPublished(this.props.publishedDateTrips, 20)}
-					</Slider>
-					<div className='title'>Trending Now</div>
-					<Slider {...explore_settings} className='explore_slider'>
-						{this.renderPublished(this.props.publishedTrendingTrips, 20)}
-					</Slider>
-					<div className='title'>Popular Trips</div>
-					<Slider {...explore_settings} className='explore_slider'>
-						{this.renderPublished(this.props.publishedPopularTrips, 20)}
-					</Slider>
-					<div className='title'>All Trips</div>
-					<Slider {...explore_settings} className='explore_slider'>
-						{this.renderPublished(this.props.publishedTrips, -1)}
-					</Slider>
+					{this.renderTripList(this.props.publishedDateTrips, 20, 'Recently Published Trips')}
+					{this.renderTripList(this.props.publishedTrendingTrips, 20,'Trending Now')}
+					{this.renderTripList(this.props.publishedPopularTrips, 20, 'Popular Trips')}
+					{this.renderTripList(this.props.publishedTrips, 20, 'All Trips')}
 				</div>			
 			</div>
 			)
