55f2b2a6fad496c26b549c0fb95c096e69c4bf43
Jiyun Sung <ji.yun.sung.18@dartmouth.edu>
2018-02-14 22:33:07 -0500

    removed a useless line

--- src/components/onboarding/index.js
+++ src/components/onboarding/index.js
@@ -95,7 +95,6 @@ class Onboarding extends Component {
 	}
 
 	onCreateTrip(startDate, endDate) {
-		//let trip_name = this.state.cities[0].name
 		let trip_name = this.state.trip_name
 		if (_.isUndefined(trip_name) || trip_name === '') { 
 			trip_name = `${this.state.cities[0].name.split(',')[0]} Trip`
