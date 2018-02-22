d2c3642838096a99cfa12eb212f1d4908af16151
Samuel Lee <sleeinsuk@gmail.com>
2018-02-21 21:10:31 -0500

    Added permission reducer to regular reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import TripsReducer from './trips_reducer'
 import CardsReducer from './cards_reducer'
 import UsersReducer from './users_reducer'
+import PermissionReducer from './permission_reducer'
 
 const rootReducer = combineReducers({
 	trips: TripsReducer,
 	cards: CardsReducer,
-	users: UsersReducer
+	users: UsersReducer,
+  permission: PermissionReducer
 });
 
 export default rootReducer;
